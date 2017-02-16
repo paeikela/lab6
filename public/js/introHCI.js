c6ba25984a7d5ad73153c6f120fc4219aeb41359
paeikela <paeikela@stud.ntnu.no>
2017-02-16 12:28:50 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,50 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	
+	$.get("project/" + idNumber, addProject);
+	console.log(idNumber);
+
+
+	function addProject(result){
+		console.log(result); 
+		var projectHTML = '<a href="#" class="thumbnail">' + '<img src="' + result['image'] + 
+		'" class="detailsImage">' + '<p>' + result['title'] + '</p>' + 
+		'<p>' + result['summary'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p/a>'; 
+
+		$(".details", idNumber);
+		
+		$("div #project"+ idNumber + " .details").html(projectHTML);
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result['summary']);
+
+
+	}
 }
+/*
+		res.json(project);
+
+		var projectHTML = '<a href= "#" class = "thumbnail">' +
+			'<img src="' + result['image'] + '"class = "img"' +
+			'<p>' + result['title'] + '</p>' + 
+			'<p><small>' + result['date'] + '</small></p></a>';
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result['summary']);
+		
+	
+
+$("" + idNumber) 
+	
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
+*/
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +78,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette",randomColor);
+
+}
+
+function randomColor(result){
+	var hex = result['colors'];
+	var colors = hex['hex'];
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
